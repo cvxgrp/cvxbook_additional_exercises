8c3d7e13b4f0a7fb94051678b1c685df61d5d0b9
mikie Reed <31933905+mikiereed@users.noreply.github.com>
2023-08-03 12:48:00 -0700

    Update ad_disp_data.py
    Update all np.random.rand(x, 1) to np.random.rand(x)

--- python/ad_disp_data.py
+++ python/ad_disp_data.py
@@ -7,13 +7,13 @@ m = 30    #number of contracts
 T = 60    #number of periods
 
 #number of impressions in each period
-I = 10*np.random.rand(T, 1)
+I = 10*np.random.rand(T)
 #revenue rate for each period and ad
 R = np.random.rand(n, T)
 #contract target number of impressions
-q = T/float(n)*50*np.random.rand(m, 1)
+q = T/float(n)*50*np.random.rand(m)
 #penalty rate for shortfall
-p = np.random.rand(m, 1)
+p = np.random.rand(m)
 #one column per contract. 1's at the periods to be displayed
 Tcontr = np.matrix(np.random.rand(T, m)>.8, dtype = float)
 Acontr = np.zeros((n, m))
